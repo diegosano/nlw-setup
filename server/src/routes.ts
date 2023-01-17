a7616d012f68e5726733ae2365c0265de7b0f6db
Diego Sano <diego.sano@nave.rs>
2023-01-17 20:52:48 -0300

    feat(server): create get day route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -27,4 +27,44 @@ export async function appRoutes(app: FastifyInstance) {
       },
     });
   });
+
+  app.get('/day', async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParams.parse(request.query);
+
+    const parsedDate = dayjs(date).startOf('day');
+    const weekDay = parsedDate.get('day');
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        createdAt: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            weekDay,
+          },
+        },
+      },
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits = day?.dayHabits.map((dayHabit) => dayHabit.habitId);
+
+    return {
+      possibleHabits,
+      completedHabits,
+    };
+  });
 }

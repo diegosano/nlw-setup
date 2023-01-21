d8b38a463a52424d87f61c40e046ca5eb61a4f6a
Diego Sano <diego.sano@nave.rs>
2023-01-21 02:11:26 -0300

    fix(mobile): habit day logic to change background color

--- mobile/src/components/HabitDay.tsx
+++ mobile/src/components/HabitDay.tsx
@@ -29,11 +29,11 @@ export function HabitDay({
         ['bg-violet-900 border-violet-700']:
           completedPercentage > 0 && completedPercentage < 20,
         ['bg-violet-800 border-violet-600']:
-          completedPercentage > 20 && completedPercentage < 40,
+          completedPercentage >= 20 && completedPercentage < 40,
         ['bg-violet-700 border-violet-500']:
-          completedPercentage > 40 && completedPercentage < 60,
+          completedPercentage >= 40 && completedPercentage < 60,
         ['bg-violet-600 border-violet-500']:
-          completedPercentage > 60 && completedPercentage < 80,
+          completedPercentage >= 60 && completedPercentage < 80,
         ['bg-violet-500 border-violet-400']: completedPercentage > 80,
         ['border-white border-4']: isCurrentDay,
       })}

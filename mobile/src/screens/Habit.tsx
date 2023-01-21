249630c72999ce3284c8d00eadc0e25e47ad8966
Diego Sano <diego.sano@nave.rs>
2023-01-21 02:12:30 -0300

    feat(mobile): add message to past date habit

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs';
+import clsx from 'clsx';
 import { useEffect, useState } from 'react';
 import { View, ScrollView, Text, Alert } from 'react-native';
 import { useRoute } from '@react-navigation/native';
@@ -33,6 +34,7 @@ export function Habit() {
   const route = useRoute();
   const { date } = route.params as Params;
   const parsedDate = dayjs(date);
+  const isDateInPast = parsedDate.endOf('day').isBefore(new Date());
   const dayOfWeek = parsedDate.format('dddd');
   const dayAndMonth = parsedDate.format('DD/MM');
   const habitsProgress =
@@ -96,7 +98,11 @@ export function Habit() {
 
         <ProgressBar progress={habitsProgress} />
 
-        <View className="mt-6">
+        <View
+          className={clsx('mt-6', {
+            'opacity-50': isDateInPast,
+          })}
+        >
           {dayInfo.possibleHabits.length > 0 ? (
             dayInfo.possibleHabits.map((habit) => (
               <Checkbox
@@ -104,12 +110,19 @@ export function Habit() {
                 title={habit.title}
                 checked={completedHabits.includes(habit.id)}
                 onPress={() => handleToggleHabit(habit.id)}
+                disabled={isDateInPast}
               />
             ))
           ) : (
             <HabitsEmpty />
           )}
         </View>
+
+        {isDateInPast && (
+          <Text className="text-white mt-10 text-center">
+            You can not toggle habits from a past date.
+          </Text>
+        )}
       </ScrollView>
     </View>
   );
